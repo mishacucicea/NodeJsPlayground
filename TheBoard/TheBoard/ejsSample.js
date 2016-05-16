d8eaada01de227d753a4a9ddb9e3bafb1212a51c
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 15:47:10 +0300

    Adding ejs sample.

---
+++ TheBoard/TheBoard/ejsSample.js
@@ -0,0 +1,16 @@
+﻿var http = require("http");
+var express = require("express");
+var app = new express();
+var ejsEngine = require("ejs-locals");
+
+// Setup the View Engine.
+app.engine("ejs", ejsEngine); // support the master pages
+app.set("view engine", "ejs"); // ejs view engine
+
+app.get("/", function (req, res) {
+	res.render("ejs/index", { title: "Express + EJS" });
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
