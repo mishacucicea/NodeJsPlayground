9d2128cd671cd4de00c457650771e0ff8b65da1d
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 16:12:54 +0300

    Transforming vashSample to use a controller.

---
+++ TheBoard/TheBoard/controllers/homeController.js
@@ -0,0 +1,8 @@
+﻿(function (homeController) { 
+	homeController.init = function (app) {
+		app.get("/", function (req, res) {
+			res.render("vash/index", { title: "Express + Vash" });
+		});
+	};
+	
+})(module.exports);
--- TheBoard/TheBoard/vashSample.js
+++ TheBoard/TheBoard/vashSample.js
@@ -1,13 +1,13 @@
 ﻿var http = require("http");
 var express = require("express");
 var app = new express();
+var controllers = require("./controllers");
 
 // Setup the View Engine.
-app.set("view engine", "vash"); // view engine
+app.set("view engine", "vash");
 
-app.get("/", function (req, res) {
-	res.render("vash/index", { title: "Express + Vash" });
-});
+// Map the routes.
+controllers.init(app);
 
 var server = http.createServer(app);
 
