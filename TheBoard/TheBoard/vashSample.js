71ec6dd8488c8414bcd69964fee73288d6ca2920
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 17:36:12 +0300

    Adding static resources.

--- TheBoard/TheBoard/vashSample.js
+++ TheBoard/TheBoard/vashSample.js
@@ -6,6 +6,9 @@ var controllers = require("./controllers");
 // Setup the View Engine.
 app.set("view engine", "vash");
 
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
 // Map the routes.
 controllers.init(app);
 
