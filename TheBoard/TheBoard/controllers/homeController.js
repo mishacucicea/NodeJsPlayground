f64106185d6655e7d91f8c3027d5e1161315026a
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 17:01:38 +0300

    Adding a service.

--- TheBoard/TheBoard/controllers/homeController.js
+++ TheBoard/TheBoard/controllers/homeController.js
@@ -1,7 +1,17 @@
-﻿(function (homeController) { 
+﻿(function (homeController) {
+	
+	var data = require("../data");
+	
 	homeController.init = function (app) {
 		app.get("/", function (req, res) {
-			res.render("vash/index", { title: "Express + Vash" });
+			
+			data.getNoteCategories(function (err, results) {
+				res.render("vash/index", {
+					title: "Express + Vash", 
+					error : err, 
+					categories : results
+				});
+			});
 		});
 	};
 	
---
+++ TheBoard/TheBoard/data/index.js
@@ -0,0 +1,9 @@
+﻿(function (data) {
+	var seedData = require("./seedData");
+
+	data.getNoteCategories = function (next) {
+		// error is null.
+		next(null, seedData.initialNotes);
+	};
+	
+})(module.exports);
