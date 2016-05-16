e967437a5ef1ec8fcca3609566ec6a41e0fe7be8
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 15:57:28 +0300

    Adding vash example.

---
+++ TheBoard/TheBoard/vashSample.js
@@ -0,0 +1,14 @@
+﻿var http = require("http");
+var express = require("express");
+var app = new express();
+
+// Setup the View Engine.
+app.set("view engine", "vash"); // view engine
+
+app.get("/", function (req, res) {
+	res.render("vash/index", { title: "Express + Vash" });
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
