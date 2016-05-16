68ff003aef74d9412ca19b882696c2f624b017cd
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 14:22:26 +0300

    Adding jade examples.

--- TheBoard/TheBoard/expressServer.js
+++ TheBoard/TheBoard/expressServer.js
@@ -2,8 +2,12 @@
 var express = require("express");
 var app = new express();
 
+// Setup the View Engine.
+app.set("view engine", "jade");
+
 app.get("/", function (req, res) {
-	res.send("<html><body><h1>Express</h1><body></html>");
+	//res.send("<html><body><h1>Express</h1><body></html>");
+	res.render("jade/index", { title: "Express + Jade" });
 });
 
 app.get("/api/users", function (req, res) {
