a53d19f97aa364e12603d0ec7fb34da45f6c9b8c
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 14:07:11 +0300

    Adding Api example.

--- TheBoard/TheBoard/expressServer.js
+++ TheBoard/TheBoard/expressServer.js
@@ -2,8 +2,13 @@
 var express = require("express");
 var app = new express();
 
-app.get("/", function (req, res) { 
+app.get("/", function (req, res) {
 	res.send("<html><body><h1>Express</h1><body></html>");
+});
+
+app.get("/api/users", function (req, res) {
+	res.set("Content-Type", "application/json");
+	res.send({ name: "Shawn", isValid: true, group : "Admin" });
 });
 
 var server = http.createServer(app);
