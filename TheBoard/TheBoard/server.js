9fa5ebb55dd7a364e75e01b910b8161253224165
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 13:39:25 +0300

    write request URL in browser

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -2,6 +2,8 @@
 
 var server = http.createServer(function (req, res) {
 	console.log(req.url);
+	res.write("<html><body><h1>" + req.url + "</h1><body></html>");
+	res.end();
 });
 
 server.listen(3000);
