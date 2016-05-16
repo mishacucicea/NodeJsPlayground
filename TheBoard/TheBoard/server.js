318c569c68960c26ee2ccf440114410fcb32472c
Mihai Cucicea <mishacucicea@yahoo.com>
2016-05-16 13:36:38 +0300

    echo request URL

--- TheBoard/TheBoard/server.js
+++ TheBoard/TheBoard/server.js
@@ -1,6 +1,7 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var http = require("http");
+
+var server = http.createServer(function (req, res) {
+	console.log(req.url);
+});
+
+server.listen(3000);
